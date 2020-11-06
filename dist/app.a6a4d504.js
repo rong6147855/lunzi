fbb339aa18f0c4583cf467e91e183660a662266f
yangrong <rong6147855@foxmail.com>
2020-11-06 10:04:11 +0800

    添加 icon 和iconPosition

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -12437,7 +12437,22 @@ exports.default = void 0;
 //
 //
 //
-var _default = {};
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//`#i-${icon}`模板字符串插值。插入一个icon .icon的值是从props获得的，
+//props的值是通过index.html icon="settings"传过来的
+//实现用户在index,html输入settings我们就给了icon
+var _default = {
+  props: ['icon', 'iconPosition'] //icon位置 left right
+
+};
 exports.default = _default;
         var $481450 = exports.default || module.exports;
       
@@ -12448,10 +12463,26 @@ exports.default = _default;
         /* template */
         Object.assign($481450, (function () {
           var render = function() {
+  var _obj
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("button", { attrs: { calss: "g-button" } }, [_vm._v("按钮")])
+  return _c(
+    "button",
+    {
+      staticClass: "g-button",
+      class: ((_obj = {}), (_obj["icon-" + _vm.iconPosition] = true), _obj)
+    },
+    [
+      _vm.icon
+        ? _c("svg", { staticClass: "icon" }, [
+            _c("use", { attrs: { "xlink:href": "#i-" + _vm.icon } })
+          ])
+        : _vm._e(),
+      _vm._v(" "),
+      _c("div", { staticClass: "content" }, [_vm._t("default")], 2)
+    ]
+  )
 }
 var staticRenderFns = []
 render._withStripped = true
@@ -12528,7 +12559,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56635" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55184" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,22 @@
 <template>
-    <button calss="g-button">按钮</button>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+         <!-- <button calss="g-button" :class="{'undefined':true}">
+         <button calss="g-button" :class="{'right':true}">
+         <button calss="g-button" :class="{'left':true}"> -->
+        <svg  v-if="icon" class="icon">
+            <use :xlink:href="`#i-${icon}`"></use></svg> 
+      <div class="content">
+        <slot></slot>
+      </div>
+    </button>
 </template>
 <script>
-export default {}
+//`#i-${icon}`模板字符串插值。插入一个icon .icon的值是从props获得的，
+            //props的值是通过index.html icon="settings"传过来的
+            //实现用户在index,html输入settings我们就给了icon
+export default {
+    props:['icon','iconPosition']//icon位置 left right
+}
 </script>
 <style lang="scss">
   .g-button{
@@ -12,6 +26,10 @@ export default {}
             border-radius: var(--border-radius);
             border: 1px solid var(--border-color);
             background: var(--button-bg);
+            display: inline-flex;
+            justify-content: center;
+            align-items: center; 
+            vertical-align: middle;        
         &:hover{
             border-color:var(--border-color-hover);
         }//&表示当前选择器
@@ -20,6 +38,23 @@ export default {}
         }
         &:focus{
             outline: none;
+        }
+        > .content{
+            order:2;
+        }
+        >.icon{
+            order:1;
+            margin-right: .1em;;
+        }
+        &.icon-right{
+            >.content{
+                order:1;
+            }
+            >.icon{
+                order:2;
+                margin-right: 0;
+                margin-left: .1em;
+            }
         }
   }
 
