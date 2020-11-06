460294bfd92191599d5a06bb6fc4432496d1d08a
yangrong <rong6147855@foxmail.com>
2020-11-06 22:21:57 +0800

    第四次连接travis

--- dist/button.test.js
+++ dist/button.test.js
@@ -12740,7 +12740,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50568" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63362" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
